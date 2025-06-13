db523fbf7a4a01b159d604e730c1a912ec9d2445
ibrahim <129620134+IbraDoesCode@users.noreply.github.com>
2025-06-13 17:16:04 +0800

    feat: register product routes in app entry point

--- services/product/src/index.ts
+++ services/product/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./utils/mongo";
+import productRoute from "./routes/product.route";
 
 dotenv.config();
 
@@ -12,6 +13,8 @@ app.use(express.json());
 app.get("/", (_, res) => {
   res.send("Hello from product service");
 });
+
+app.use("/product", productRoute);
 
 app.listen(PORT, async () => {
   await connectDB();
