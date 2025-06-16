ab9e4a89d9f289fd7a0813a6a7954b70fd1794c4
ibrahim <129620134+IbraDoesCode@users.noreply.github.com>
2025-06-16 19:18:03 +0800

    refactor: simplify and standardize HTTPResponse utility with common status methods
    - Replaced HTTPResponse class with a functional object
    - Added specific methods for common response types: ok, badRequest, notFound, internalServerError
    - Improves readability and consistency across response handling

--- services/product/src/utils/http-response.ts
+++ services/product/src/utils/http-response.ts
@@ -1,27 +1,23 @@
 import { Response } from "express";
 
-export class HTTPResponse {
-  static success<T>(
-    res: Response,
-    statusCode: number,
-    message: string,
-    data: T
-  ) {
-    return res
-      .status(statusCode)
-      .json({ success: true, message: message, data: data });
-  }
+export const HTTPResponse = {
+  ok<T>(res: Response, message: string, data: T) {
+    return res.status(200).json({ success: true, message, data });
+  },
 
-  static error(
+  badRequest<T>(res: Response, message = "Bad Request", errorDetails?: T) {
+    return res.status(400).json({ success: false, message, errorDetails });
+  },
+
+  notFound<T>(res: Response, message = "Not found", errorDetails?: T) {
+    return res.status(404).json({ success: false, message, errorDetails });
+  },
+
+  internalServerError<T>(
     res: Response,
-    statusCode: number,
-    message: string,
-    errorDetails?: any
+    message = "Internal Server Error",
+    errorDetails?: T
   ) {
-    return res.status(statusCode).json({
-      success: false,
-      message: message,
-      error: errorDetails ?? null,
-    });
-  }
-}
+    return res.status(500).json({ success: false, message, errorDetails });
+  },
+};
