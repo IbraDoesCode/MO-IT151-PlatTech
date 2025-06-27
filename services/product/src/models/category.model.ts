0028d5ef0f2fd4c861a57b286116e08f7f1ed090
Morfusee <markvalenzuela.coding@gmail.com>
2025-06-28 02:10:29 +0800

    fix: removed url field for category

--- services/product/src/models/category.model.ts
+++ services/product/src/models/category.model.ts
@@ -3,14 +3,12 @@ import { model, Schema } from "mongoose";
 interface ICategory {
   slug: string;
   name: string;
-  url: string;
 }
 
 const categorySchema = new Schema<ICategory>(
   {
     slug: { type: String, required: true, unique: true },
     name: { type: String, required: true, unique: true },
-    url: { type: String, required: true, unique: true },
   },
   { timestamps: true, versionKey: false }
 );
