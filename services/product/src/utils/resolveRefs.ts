4d9aeb0ef81c2ea7b23b9b19b3c96f95411c78f6
Morfusee <markvalenzuela.coding@gmail.com>
2025-06-28 02:10:51 +0800

    feat: added resolver for objectid

--- services/product/src/utils/resolveRefs.ts
+++ services/product/src/utils/resolveRefs.ts
@@ -19,15 +19,26 @@ export const resolveCategory = async (slug: string) => {
     .map((s) => s.charAt(0).toUpperCase() + s.slice(1))
     .join(" ");
 
-  const url = `http://localhost:3000/product?category=${slug}`;
-
   const category = await Category.findOneAndUpdate(
     { slug },
     {
-      $setOnInsert: { name, url }, // Only set name and url if inserting
+      $setOnInsert: { name }, // Only set name if inserting
     },
     { upsert: true, new: true }
   );
 
   return category;
+};
+
+export const resolveValueToObjectId = async (
+  model: any,
+  field: string,
+  value: string
+) => {
+  const query = {
+    [field]: { $regex: value, $options: "i" },
+  };
+
+  const doc = await model.findOne(query);
+  return doc ? doc._id : null;
 };
